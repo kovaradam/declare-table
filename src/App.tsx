6124ef8abee0bac7200619bcbec3a7208cd7ed04
Adam Kovář <kovaradam1@gmail.com>
2022-03-27 15:18:42 +0200

    create data cell

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,35 @@
 import React from "react";
 
 export function App() {
+  const [checked, setChecked] = React.useState("all");
   const data = [
-    { name: "name-1", type: "type-1" },
-    { name: "name-2", type: "type-2" },
+    { name: "name-1", type: "type-1", value: "1" },
+    { name: "name-2", type: "type-2", value: "2" },
   ];
   return (
     <Table data={data}>
-      <Column<typeof data[0]>
-        dataKey="name"
-        label="name"
-        cell={(entry) => entry.type.toUpperCase()}
-      />
+      {true && (
+        <Column<typeof data[0]>
+          id="check"
+          label={
+            <input
+              value="all"
+              type="checkbox"
+              checked={checked == "all"}
+              onChange={(event) => setChecked(event.target.value)}
+            />
+          }
+          cell={(entry) => (
+            <input
+              type="checkbox"
+              value={entry.value}
+              checked={[entry.value, "all"].includes(checked)}
+              onChange={(event) => setChecked(event.target.value)}
+            />
+          )}
+        />
+      )}
+      <Column<typeof data[0]> dataKey="name" label="name" />
       <Column<typeof data[0]>
         dataKey="type"
         label={<code>type</code>}
@@ -21,9 +39,15 @@ export function App() {
   );
 }
 
+type TableChild<DataType> =
+  | React.ReactElement<ColumnProps<DataType>>
+  | boolean
+  | null
+  | undefined;
+
 type Props<DataType> = {
   data: DataType[];
-  children: React.ReactElement<ColumnProps<DataType>, typeof Column>[];
+  children: TableChild<DataType>[];
 };
 
 function Table<DataType extends Record<string, unknown>>({
@@ -31,17 +55,17 @@ function Table<DataType extends Record<string, unknown>>({
   ...props
 }: Props<DataType>) {
   const columns = React.Children.map(children, (child) => {
-    if (!child) {
+    if (!isColumnComponent(child)) {
       return;
     }
-    if (typeof child !== "object" || !("type" in child)) {
-      throw new Error("Table children must be react nodes");
-    }
-    if (typeof child.type === "string" || child.type.name !== Column.name) {
-      throw new Error("Table children must be Column components");
-    }
 
-    return { ...child.props };
+    const props = child.props;
+
+    return {
+      ...props,
+      render: (entry: DataType) => <DataCell {...child.props} entry={entry} />,
+      _id: String(props.id ?? props.dataKey ?? props.label),
+    };
   });
 
   return (
@@ -49,17 +73,17 @@ function Table<DataType extends Record<string, unknown>>({
       <thead>
         <tr>
           {columns?.map((column) => (
-            <th key={column.dataKey}>{column.label}</th>
+            <th key={column._id}>{column.label}</th>
           ))}
         </tr>
       </thead>
       <tbody>
         {props.data.map((entry) => (
-          <tr>
-            {columns.map((column) => (
-              <td>
-                <>{column.cell ? column.cell(entry) : entry[column.dataKey]}</>
-              </td>
+          <tr key={JSON.stringify(entry)}>
+            {columns?.map((column) => (
+              <React.Fragment key={column._id}>
+                {column.render(entry)}
+              </React.Fragment>
             ))}
           </tr>
         ))}
@@ -69,7 +93,8 @@ function Table<DataType extends Record<string, unknown>>({
 }
 
 type ColumnProps<DataType> = {
-  dataKey: keyof DataType & string;
+  dataKey?: keyof DataType & string;
+  id?: string | number;
   label: React.ReactNode;
   cell?: (entry: DataType) => React.ReactNode;
   children?: (entry: DataType) => React.ReactNode;
@@ -80,3 +105,38 @@ const Column = <DataType extends Record<string, unknown>>(
 ) => {
   return null;
 };
+
+const DataCell = <DataType extends Record<string, unknown>>(
+  props: ColumnProps<DataType> & { entry: DataType }
+): React.ReactElement => {
+  const renderCell = props.cell ?? props.children;
+  if (!renderCell) {
+    if (props.dataKey === undefined) {
+      throw { type: "No data key nor render cell function specified" };
+    }
+    return (
+      <td>
+        <>{props.entry[props.dataKey]}</>
+      </td>
+    );
+  }
+  return <td>{renderCell(props.entry)}</td>;
+};
+
+function isColumnComponent<DataType>(
+  component: React.ReactNode
+): component is React.ReactElement<ColumnProps<DataType>> {
+  if (!component) {
+    return false;
+  }
+  if (typeof component !== "object" || !("type" in component)) {
+    throw new Error("Table children must be react nodes");
+  }
+  if (
+    typeof component.type === "string" ||
+    component.type.name !== Column.name
+  ) {
+    throw new Error("Table children must be Column components");
+  }
+  return true;
+}
